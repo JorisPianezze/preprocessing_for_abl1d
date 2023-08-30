e63b04be8a98df683c19ab46a3fb1be7bc8e5d6e
JorisP <pianezze.joris@gmail.com>
2023-08-30 09:57:41 +0200

    Rename examples directories and add scripts to convert preprocessing outputs to CROCO format

---
+++ examples/benguela_ecmwf_era5/2_convert_to_croco/create_croco_abl_from_ifs_era5.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------
+#       Auteur (date de creation)
+#  J. PIANEZZE (      26.07.2022)
+# ----------------------------------------------------
+#
+print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+print('&&&                                           ')
+print('&&&   Execution de :                          ')
+print('&&&  ',__file__                                )
+print('&&&                                           ')
+print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+#
+######################################################
+###                                           
+import os, sys
+import netCDF4
+import numpy as np
+import xarray
+import datetime
+###                                           
+######################################################
+
+cfg_debug           = True
+
+cfg_dir_input_files = '../1_run_preprocessing_tool_N_abl_50/'
+
+# ~~~ Has to be the same as extract_era5_for_abl1d.py ~~~
+first_date          = datetime.datetime(2014,11, 6, 0, 0, 0)
+last_date           = datetime.datetime(2014,11, 9,18, 0, 0)
+period_in_hr        = 6
+
+if cfg_debug : print('++++++++++++++++++++++++++++++++++++++++++++++')
+if cfg_debug : print('+++                                           ')
+if cfg_debug : print('+++   0. Lecture des fichiers et des variables')
+if cfg_debug : print('+++                                           ')
+if cfg_debug : print('++++++++++++++++++++++++++++++++++++++++++++++')
+
+# ---------------------------------------------------------------------
+#  Get datasets
+# ---------------------------------------------------------------------
+file_croco_grd = netCDF4.Dataset('croco_grd.nc')
+file_abl_grd   = netCDF4.Dataset(cfg_dir_input_files+'vertical_grid_N_abl_50_H_2000m_DZ_10m.nc')
+file_abl_ifs   = netCDF4.Dataset(cfg_dir_input_files+'out_drown_era5_for_preprocessing_tools.nc')
+
+# ---------------------------------------------------------------------
+#  Read ABL vertical grid
+# ---------------------------------------------------------------------
+N_abl   = file_abl_grd.dimensions['jpka'] ; N_abl = N_abl.size - 1
+
+zr_abl  = file_abl_grd.variables['ght'][:N_abl]
+zw_abl  = file_abl_grd.variables['ghw'][:N_abl]
+Hzr_abl = file_abl_grd.variables['e3t'][:N_abl]
+Hzw_abl = file_abl_grd.variables['e3w'][:N_abl]
+
+# ---------------------------------------------------------------------
+#  Read IFS forcing time
+# ---------------------------------------------------------------------
+time_abl = file_abl_ifs.variables['time'] ; ntime_abl = time_abl.size
+
+# ---------------------------------------------------------------------
+#  Read CROCO dimensions
+# ---------------------------------------------------------------------
+xi_rho  = file_croco_grd.dimensions['xi_rho']  ; xi_rho  = xi_rho.size
+eta_rho = file_croco_grd.dimensions['eta_rho'] ; eta_rho = eta_rho.size
+
+if cfg_debug : print('++++++++++++++++++++++++++++++++++++++++++++++')
+if cfg_debug : print('+++                                           ')
+if cfg_debug : print('+++   1. Interpolation grille IFS a CROCO     ')
+if cfg_debug : print('+++                                           ')
+if cfg_debug : print('++++++++++++++++++++++++++++++++++++++++++++++')
+
+# --------------------------------------------
+#   Read CROCO grd
+# ---------------------------------------------
+ds_croco_grd = xarray.open_dataset('croco_grd.nc')
+lon_rho      = ds_croco_grd.lon_rho # - 360.0
+lat_rho      = ds_croco_grd.lat_rho
+
+# --------------------------------------------
+#   Read IFS grd
+# ---------------------------------------------
+ds_ifs       = xarray.open_dataset(cfg_dir_input_files+'../0_input_data/flux_from_2014-11-05_at_07_UTC_to_2014-11-10_at_06_UTC_every_01hr.grib.nc')
+ds_abl_ifs   = xarray.open_dataset(cfg_dir_input_files+'out_drown_era5_for_preprocessing_tools.nc')
+
+# --------------------------------------------
+#   Read IFS variables
+# ---------------------------------------------
+tair_ifs     = ds_abl_ifs.tair
+rhum_ifs     = ds_abl_ifs.humi
+uwnd_ifs     = ds_abl_ifs.uwnd
+vwnd_ifs     = ds_abl_ifs.vwnd
+uhpg_ifs     = ds_abl_ifs.uhpg
+vhpg_ifs     = ds_abl_ifs.vhpg
+prate_ifs    = ds_ifs.tp
+radlw_ifs    = ds_ifs.str
+radlw_in_ifs = ds_ifs.strd
+radsw_ifs    = ds_ifs.ssr
+
+# --------------------------------------------
+#   Special treatment for fluxes and precipitation
+# ---------------------------------------------
+# --> 6h-average, period_in_hr between two forcing files
+prate_ifs    = prate_ifs.resample   (time=str(period_in_hr)+'H',label='right').mean()
+radlw_ifs    = radlw_ifs.resample   (time=str(period_in_hr)+'H',label='right').mean()
+radlw_in_ifs = radlw_in_ifs.resample(time=str(period_in_hr)+'H',label='right').mean()
+radsw_ifs    = radsw_ifs.resample   (time=str(period_in_hr)+'H',label='right').mean()
+
+# --> date selection : date_first -> date_last
+prate_ifs    = prate_ifs.sel   (time=slice(first_date, last_date))
+radlw_ifs    = radlw_ifs.sel   (time=slice(first_date, last_date))
+radlw_in_ifs = radlw_in_ifs.sel(time=slice(first_date, last_date))
+radsw_ifs    = radsw_ifs.sel   (time=slice(first_date, last_date))
+
+# --------------------------------------------
+#   Interpolate at rho-points
+# ---------------------------------------------
+tair_abl     = tair_ifs.interp    (lat=lat_rho, lon=lon_rho, method='linear')
+rhum_abl     = rhum_ifs.interp    (lat=lat_rho, lon=lon_rho, method='linear')
+uwnd_abl     = uwnd_ifs.interp    (lat=lat_rho, lon=lon_rho, method='linear')
+vwnd_abl     = vwnd_ifs.interp    (lat=lat_rho, lon=lon_rho, method='linear')
+uhpg_abl     = uhpg_ifs.interp    (lat=lat_rho, lon=lon_rho, method='linear')
+vhpg_abl     = vhpg_ifs.interp    (lat=lat_rho, lon=lon_rho, method='linear')
+prate_abl    = prate_ifs.interp   (lat=lat_rho, lon=lon_rho, method='linear')
+radlw_abl    = radlw_ifs.interp   (lat=lat_rho, lon=lon_rho, method='linear')
+radlw_in_abl = radlw_in_ifs.interp(lat=lat_rho, lon=lon_rho, method='linear')
+radsw_abl    = radsw_ifs.interp   (lat=lat_rho, lon=lon_rho, method='linear')
+
+# ----------------------
+#   remove negative prate
+# -----------------------
+prate_abl = np.where(prate_abl<0.0, 0.0, prate_abl)
+
+if cfg_debug : print('++++++++++++++++++++++++++++++++++++++++++++++')
+if cfg_debug : print('+++                                           ')
+if cfg_debug : print('+++   2. Ecriture du fichier netcdf           ')
+if cfg_debug : print('+++                                           ')
+if cfg_debug : print('++++++++++++++++++++++++++++++++++++++++++++++')
+
+file_croco_abl_ifs=netCDF4.Dataset(os.getcwd()+'/croco_abl_from_ifs_era5_N_abl_'+str(N_abl)+'_interp_linear.nc','w')
+file_croco_abl_ifs.Description='Atmospheric forcing file for ABL/CROCO'
+
+# ----------------------------------
+# Create the dimensions of the files
+# ----------------------------------
+file_croco_abl_ifs.createDimension ('xi_rho',    xi_rho)
+file_croco_abl_ifs.createDimension ('eta_rho',   eta_rho)
+file_croco_abl_ifs.createDimension ('N_abl',     N_abl)
+file_croco_abl_ifs.createDimension ('bulk_time', None )
+
+# ----------------------------------
+# Create the variables of the files
+# ----------------------------------
+varout = file_croco_abl_ifs.createVariable('bulk_time','d',('bulk_time')                              ) ; varout.long_name = 'bulk formulation execution time' ; varout.units = 'days'    ; varout.cycle_length = 0.
+varout = file_croco_abl_ifs.createVariable('tair'     ,'d',('bulk_time', 'N_abl', 'eta_rho', 'xi_rho')) ; varout.long_name = 'Air temperature'                 ; varout.units = 'Celsius'
+varout = file_croco_abl_ifs.createVariable('rhum'     ,'d',('bulk_time', 'N_abl', 'eta_rho', 'xi_rho')) ; varout.long_name = 'Air specific humidity'           ; varout.units = 'kg/kg'
+varout = file_croco_abl_ifs.createVariable('prate'    ,'d',('bulk_time',          'eta_rho', 'xi_rho')) ; varout.long_name = 'Precipitation rate'              ; varout.units = 'cm/day'
+varout = file_croco_abl_ifs.createVariable('radlw'    ,'d',('bulk_time',          'eta_rho', 'xi_rho')) ; varout.long_name = 'Net longwave radiation'          ; varout.units = 'W/m2'
+varout = file_croco_abl_ifs.createVariable('radlw_in' ,'d',('bulk_time',          'eta_rho', 'xi_rho')) ; varout.long_name = 'Downward longwave radiation'     ; varout.units = 'W/m2'
+varout = file_croco_abl_ifs.createVariable('radsw'    ,'d',('bulk_time',          'eta_rho', 'xi_rho')) ; varout.long_name = 'Net shortwave radiation'         ; varout.units = 'W/m2'
+varout = file_croco_abl_ifs.createVariable('uhpg'     ,'d',('bulk_time', 'N_abl', 'eta_rho', 'xi_rho')) ; varout.long_name = 'u-hpg'                           ; varout.units = 'm/s'
+varout = file_croco_abl_ifs.createVariable('vhpg'     ,'d',('bulk_time', 'N_abl', 'eta_rho', 'xi_rho')) ; varout.long_name = 'v-hpg'                           ; varout.units = 'm/s'
+varout = file_croco_abl_ifs.createVariable('uwnd'     ,'d',('bulk_time', 'N_abl', 'eta_rho', 'xi_rho')) ; varout.long_name = 'u-wind speed'                    ; varout.units = 'm/s'
+varout = file_croco_abl_ifs.createVariable('vwnd'     ,'d',('bulk_time', 'N_abl', 'eta_rho', 'xi_rho')) ; varout.long_name = 'v-wind speed'                    ; varout.units = 'm/s'
+varout = file_croco_abl_ifs.createVariable('zr'       ,'d',(             'N_abl'                     )) ; varout.long_name = 'Altitude of t-level'             ; varout.units = 'm'
+varout = file_croco_abl_ifs.createVariable('zw'       ,'d',(             'N_abl'                     )) ; varout.long_name = 'Altitude of w-level'             ; varout.units = 'm'
+varout = file_croco_abl_ifs.createVariable('Hzr'      ,'d',(             'N_abl'                     )) ; varout.long_name = 'Thickness of t-level'            ; varout.units = 'm'
+varout = file_croco_abl_ifs.createVariable('Hzw'      ,'d',(             'N_abl'                     )) ; varout.long_name = 'Thickness of w-level'            ; varout.units = 'm'
+
+# ---------------------------------------
+# Write out the data arrays into the file
+# ---------------------------------------
+file_croco_abl_ifs.variables['bulk_time'][:      ] = time_abl     [:      ]/24.0              # from hours to days
+file_croco_abl_ifs.variables['tair']     [:,:,:,:] = tair_abl     [:,:-1,:,:]-273.15          # from K to C
+file_croco_abl_ifs.variables['rhum']     [:,:,:,:] = rhum_abl     [:,:-1,:,:]
+file_croco_abl_ifs.variables['uhpg']     [:,:,:,:] = uhpg_abl     [:,:-1,:,:]
+file_croco_abl_ifs.variables['vhpg']     [:,:,:,:] = vhpg_abl     [:,:-1,:,:]
+file_croco_abl_ifs.variables['uwnd']     [:,:,:,:] = uwnd_abl     [:,:-1,:,:]
+file_croco_abl_ifs.variables['vwnd']     [:,:,:,:] = vwnd_abl     [:,:-1,:,:]
+file_croco_abl_ifs.variables['zr']       [  :    ] = zr_abl       [  :    ]
+file_croco_abl_ifs.variables['zw']       [  :    ] = zw_abl       [  :    ]
+file_croco_abl_ifs.variables['Hzr']      [  :    ] = Hzr_abl      [  :    ]
+file_croco_abl_ifs.variables['Hzw']      [  :    ] = Hzw_abl      [  :    ]
+
+file_croco_abl_ifs.variables['prate']    [:,  :,:] = prate_abl    [:,  :,:]*1E2/(1.0/24.0)/1E3 # from kg/m2 to cm/day
+file_croco_abl_ifs.variables['radlw']    [:,  :,:] = radlw_abl    [:,  :,:]/(1.0*3600.0)       # from J/m2 to W/m2
+file_croco_abl_ifs.variables['radlw_in'] [:,  :,:] = radlw_in_abl [:,  :,:]/(1.0*3600.0)       # from J/m2 to W/m2
+file_croco_abl_ifs.variables['radsw']    [:,  :,:] = radsw_abl    [:,  :,:]/(1.0*3600.0)       # from J/m2 to W/m2 
+
+# ---------------------------------------
+# close the file
+# ---------------------------------------
+file_croco_abl_ifs.close()
+
+print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+print('&&&                                           ')
+print('&&&   Fin de :                                ')
+print('&&&  ',__file__                                )
+print('&&&                                           ')
+print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
--- readme.md
+++ readme.md
@@ -1,72 +1,112 @@
 <!-- ========================================= -->
+
 # Contents
+
 <!-- ========================================= -->
 
 1. [Repository description](#Description)
 2. [Compilation](#Compilation)
 3. [Usage](#Usage)
 4. [Supplementary material](#Supplementary)
+5. [Reference](#Reference)
 
 <!-- ========================================= -->
+
 # 1. Repository description <a name="Description"></a>
+
 <!-- ========================================= -->
 
-This repository contains the tool to pre-process atmospheric fields for the atmospheric boundary layer model (ABL1D). This tool can read atmospheric fields from ECMWF products (IFS oper, ERA5, ERAI, ...). It allows to compute geostrophic pressure gradients, to interpolate the fields from the IFS vertical grid to the ABL1D vertical grid (which is defined in namelist) and to fill in the field values over land by extrapolating the values over sea.
+This Git's repository contains the tools to pre-process atmospheric fields for the atmospheric boundary layer model (ABL1d ; Lemarié et al. 2021) which has been recently included in NEMO and CROCO oceanic models. This tool can read atmospheric fields from ECMWF products (IFS oper, ERA5, ERAI, ...). It allows to interpolate the fields from the IFS vertical grid to the ABL1d vertical grid (which is defined in namelist), to compute geostrophic pressure gradients, and to fill in the field values over land by extrapolating the values over sea.
 
-This repository contains the following files:
+This repository contains the following directories :
+
 * **src**: contains the Makefile and the source codes in Fortran 90 ;
-* **ktest**: contains the files necessary to launch the tool and prepare the ABL1d forcing for CROCO BENGUELA_LR case study.
+* **examples**: contains examples to extract and launch the tools.
 
 <!-- ========================================= -->
+
 # 2. Compilation <a name="Compilation"></a>
+
 <!-- ========================================= -->
- 
+
 To compile the source code, go to the src/ folder and run the command :
+
 ```bash
 make
 ```
 
 4 executables are created : 
-* get_atm_LSfrc.exe: program to calculate the pressure gradients;
-* vinterp_atm_frc.exe : program to vertically interpolate the IFS fields to the ABL1d;
-* drown_atm_frc.exe: program to fill in the values on land by extrapolating the values on sea (avoid strong gradients near the coast);
-* slp_smoothing.exe : program to test different methods to smooth the surface pressure (Gibbs effect). Using this program is not necessary if you only want to create the forcing for ABL1d.
+
+* **get_atm_LSfrc.exe**: program to calculate the large scale pressure gradients;
+* **vinterp_atm_frc.exe** : program to vertically interpolate the IFS fields to the ABL1d;
+* **drown_atm_frc.exe**: program to fill in the values on land by extrapolating the values on sea (avoid strong gradients near the coast);
+* **slp_smoothing.exe** : program to test different methods to smooth the surface pressure (Gibbs effect). Using this program is not necessary if you only want to create the forcing for ABL1d.
 
 To clean up the compiled code, use the command :
+
 ```bash
 make clean
 ```
 
 <!-- ========================================= -->
+
 # 3. Usage <a name="Usage"></a>
+
 <!-- ========================================= -->
 
-To test the tool, go to the ktest/benguela_ifs_era5 folder. This folder contains all the files and scripts necessary for the
-preprocessing tool. It contains the following folders: 
-* **0_input_data**: contains the input files for the preprocessing tool. They correspond to the IFS files extracted from the ECMWF servers (in grib format).
-* **1_run_preprocessing_tool_N_abl_50**: contains the scripts to run the pre-processing tool previously compiled.
+To test the tool, go to the examples/benguela_ecmwf_era5/ folder corresponding to a test case for CROCO. This directory contains all the files and scripts necessary to run the preprocessing tool. It contains the following directories: 
 
-To use the pre-processing tool on files containing only one date per file (0_input_data/ecmwf.20141106.00.nc, ...), the script :
+* **0_input_data**: contains ane exemple of a script to extract ERA5 data. In this example, ERA5 data are extracted every 6 hours from 6 november 2014 at 00 UTC to 9 november 2014 at 18 UTC, .
+* **1_run_preprocessing_tool_N_abl_50**: contains the scripts to run the pre-processing tools previously compiled.
+
+First, go to **0_input_data** directory and extract ERA5 fields using :
+
+```bash
+python extract_era5_for_abl1d.py
+```
+
+Then you need to convert extracted grib files into netcdf using :
+
+```bash
+./convert_grib_to_netcdf.sh
+```
+
+At the end, you must have a file called era5_for_preprocessing_tools.nc into your **0_input_data** directory. This file contains all necessary informations for pre-proccessing tools. Then, go to the **1_run_preprocessing_tool_N_abl_50** directory and run the pre-processing tool via :
+
 ```bash
 ./run_preprocessing.sh
 ```
 
-**Note:**
-* You must first convert the grib files to netcdf using the script (convert_grib_to_netcdf_with_cdo.sh) located in input_data directory.
+At the end of pre-proccessing tools executions, you need to have new files called :
 
-To use the tool on a netcdf file containing all the dates (input_data/concatenate_all_ecmwf_files.nc), use the script :
+* **geos_era5_for_preprocessing_tools.nc**: contains large scale geostrophic winds or pressure
+
+* **out_era5_for_preprocessing_tools.nc**: contains all 3d atmospheric fields without drowning over land
+
+* **out_drown_era5_for_preprocessing_tools.nc**: contains all 3d atmospheric fields with drowning over land
+
+* **vertical_grid_N_abl_50_H_2000m_DZ_10m.nc**: contains vertical grid for ABL1d
+
+**Note**: Only **out_drown_era5_for_preprocessing_tools.nc** and **vertical_grid_N_abl_50_H_2000m_DZ_10m.nc** are used to prepare forcing for NEMO or CROCO oceanic models.
+
+Now, go to **2_convert_to_croco** directory to convert preproccess file to a readable format for CROCO using :
+
 ```bash
-./run_all_preprocessing.sh
+python create_croco_abl_from_ifs_era5.py
 ```
-**Note:**
-* You must first concatenate the netcdf files using the script (concatenate_ecmwf_files.sh.sh) located in input_data.
 
+This script performs an horizontal interpolation of ERA5 fields on CROCO grid and rename variables.
+
+The new file created is called **croco_abl_from_ifs_era5_N_abl_50_interp_linear.nc** and can be used directly to force CROCO using (#undef ONLINE).
 
 <!-- ========================================= -->
+
 # 4. Supplementary material <a name="Supplementary"></a>
+
 <!-- ========================================= -->
 
-To test the smoothing of the SLP, you can also test gcm-filters. Here are some commands to install python / gcm-filters if needed (with conda) and test some scripts :
+To test the smoothing of the Sea Level Pressure, you can also test gcm-filters. Here are some commands to install python / gcm-filters if needed (with conda) and test some scripts :
+
 ```bash
 https://gcm-filters.readthedocs.io/en/latest/how_to_contribute.html
 conda create -n gcm-filters-env -c conda-forge --file requirements.txt --file requirements-dev.txt
@@ -75,3 +115,10 @@ conda install ipython
 conda install basemap
 ```
 
+<!-- ========================================= -->
+
+# 5. Reference <a name="Reference"></a>
+
+<!-- ========================================= -->
+
+Lemarié, F., Samson, G., Redelsperger, J.-L., Giordani, H., Brivoal, T., and Madec, G.: A simplified atmospheric boundary layer model for an improved representation of air–sea interactions in eddying oceanic models: implementation and first evaluation in NEMO (4.0), Geosci. Model Dev., 14, 543–572, https://doi.org/10.5194/gmd-14-543-2021, 2021.
